a28996e448daadd681ca8c3624d3632ce0614cc2
Izzanabilarosananda <30432565+Izzanabilarosananda@users.noreply.github.com>
2017-10-14 06:47:08 -0700

    Create hasil.php

---
+++ hasil.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<title>Registrasi Sukses</title>
+</head>
+<body>
+<table align="center">
+<h1><center>Registrasi Sukses</center></h1>
+<hr width="20%">
+<?php
+$email=$_POST['email'];
+$fullname=$_POST['fullname'];
+$username=$_POST['username'];
+$password=$_POST['password'];
+$password = str_repeat("*", strlen($password));
+echo "<tr><td>Email</td>
+						<td> : </td>
+						<td>$email</td>
+		</tr>";
+echo "<tr><td>Fullname</td>
+							<td> : </td>
+							<td>$fullname</td>
+		</tr>";
+echo "<tr><td>Username</td>
+							<td> : </td>
+							<td>$username</td>
+		</tr>";
+echo "<tr><td>Password</td>
+							<td> : </td>
+							<td>$password</td>
+		</tr>";
+?>
+</table>
+</body>
+</html>
